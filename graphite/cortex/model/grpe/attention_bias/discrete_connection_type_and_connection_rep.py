dd7aa2e9b754dd576308bd7d35007d51238d72e1
Shayan Fazeli <shayan@cs.ucla.edu>
2022-09-28 12:53:52 -0700

    discrete connection type and connection reps - e.g. 3d use case

--- graphite/cortex/model/grpe/attention_bias/discrete_connection_type_and_connection_rep.py
+++ graphite/cortex/model/grpe/attention_bias/discrete_connection_type_and_connection_rep.py
@@ -1,19 +1,31 @@
-from .base import AttentionBiasBase
+from overrides import overrides
 import torch
 import torch.nn
 from typing import Tuple, List
 from .discrete_connection_type import DiscreteConnectionTypeEmbeddingAttentionBias
 
 
-class DiscreteConnectionTypeEmbeddingPlusConnectionRepresentationAttentionBias(DiscreteConnectionTypeEmbeddingAttentionBias):
+class DiscreteConnectionTypeEmbeddingPlusConnectionRepresentationAttentionBias(
+    DiscreteConnectionTypeEmbeddingAttentionBias
+):
     """
-    Corresponds to the biases such as $\text{bias}^{\text{edge}}$ in the GRPE work.
+    In addition to the usual `tokenized` representation and biases in grpe (which are implemented
+    in :cls:`DiscreteConnectionTypeEmbeddingAttentionBias`, this module can consider continuous edge reps as well.
+
+    The way it works is that, in addition to the represented edge types, we also have `edge_reps` which
+    are of `dim=(batch_size, num_edges, model_dim)`, along with COO format edges. They will be projected
+    (since they are not long integers to be embedded) and that projection for query and key will
+    be used to guide the bias, meaning that we would have a $qE_{q} + kE_{k}$ this way as well.
+
+    One potential use for this could be to combine the 2d reps with the 3d-based
+    continuous edge representations for a 3d-molecule representation.
     """
     def __init__(
             self,
             *args,
             **kwargs
     ):
+        """constructor"""
         super(DiscreteConnectionTypeEmbeddingPlusConnectionRepresentationAttentionBias, self).__init__(*args, **kwargs)
         self.projection_query = torch.nn.Sequential(
             torch.nn.ReLU(),
@@ -28,15 +40,15 @@ class DiscreteConnectionTypeEmbeddingPlusConnectionRepresentationAttentionBias(D
             torch.nn.Linear(self.model_dim // self.num_heads, self.model_dim // self.num_heads),
         )
 
+    @overrides()
     def forward(
             self,
             queries: torch.Tensor,
             keys: torch.Tensor,
             edge_types: torch.LongTensor,
             edges: List[Tuple[torch.LongTensor, torch.LongTensor]],
-            edge_reps: List[torch.Tensor],
-            layer_index: int = None
-    ):
+            edge_reps: List[torch.Tensor]
+    ) -> torch.Tensor:
         """
         Parameters
         ----------
@@ -51,6 +63,9 @@ class DiscreteConnectionTypeEmbeddingPlusConnectionRepresentationAttentionBias(D
             If this is being used, we need to note that in the `edge_types` which are rendered (originally), the indices
             can go from 0 to 23. In this, given that we have the special edge types too.
 
+        edges: `List[Tuple[torch.LongTensor, torch.LongTensor]]`, required
+            The coordinate representation of edges (COO format)
+
         edge_reps: `torch.Tensor`, required
             The representations of `dim=(batch_size, num_edges, model_dim)`
         """
@@ -60,17 +75,17 @@ class DiscreteConnectionTypeEmbeddingPlusConnectionRepresentationAttentionBias(D
         bias_by_types = super().forward(
             queries=queries,
             keys=keys,
-            edge_types=edge_types,
-            layer_index=layer_index
+            edge_types=edge_types
         )  # dim=b,h,n,n
 
         for i, ((u, v), z) in enumerate(zip(edges, edge_reps)):
-            edge_biases_query = torch.sum(self.projection_query(
-                z.view(u.shape[0], num_heads, rep_dim)
-            ).transpose(0, 1) * queries[i, :, u, :], dim=-1)  # m, h, rep_dim X h, m, rep_dim
-            edge_biases_key = torch.sum(self.projection_key(
-                z.view(u.shape[0], num_heads, rep_dim)
-            ).transpose(0, 1) * keys[i, :, v, :], dim=-1)  # m, h, rep_dim X h, m, rep_dim
+            edge_biases_query = torch.bmm(
+                self.projection_query(z.view(u.shape[0], num_heads, rep_dim)).transpose(0, 1),  # num_head, num_edges, rep_dim
+                queries[i, :, u, :].transpose(-1, -2),  # num_heads, rep_dim, num_edges
+            )  # num_heads, num_edges, num_edges
+            edge_biases_key = torch.bmm(
+                self.projection_key(z.view(u.shape[0], num_heads, rep_dim)).transpose(0, 1),  # num_head, num_edges, rep_dim
+                keys[i, :, v, :].transpose(-1, -2))  # num_heads, num_edges, num_edges
             bias_by_types[i, :, u, v] = bias_by_types[i, :, u, v] + edge_biases_query + edge_biases_key
 
         return bias_by_types
