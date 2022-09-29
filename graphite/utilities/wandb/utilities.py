e9f3da2f5055caae0399eedabe4cfdb8bc8b2067
Shayan Fazeli <shayan@cs.ucla.edu>
2022-09-29 05:50:38 -0700

    wandb init update

--- graphite/utilities/wandb/utilities.py
+++ graphite/utilities/wandb/utilities.py
@@ -1,4 +1,6 @@
+from typing import Dict, Any
 import argparse
+import os
 import wandb
 import wandb.util
 
@@ -7,15 +9,28 @@ from graphite.utilities.logging.logger import get_logger
 logger = get_logger(__name__)
 
 
-def initialize_wandb(args: argparse.Namespace) -> None:
+def initialize_wandb(args: argparse.Namespace, config: Dict[str, Any] = None) -> None:
+    """
+    Initializing the wandb session
+
+    Parameters
+    ----------
+    args: `argparse.Namespace`, required
+        Arguments that are parsed from `graphite_train` script.
+
+    config: `Dict[str, Any]`, optional (default=None)
+        The parsed configuration
+    """
     if args.id is None:
         assert not args.resume, "you have chosen to resume a training process, therefore," \
                                 "please provide the corresponding wandb to resume logging."
         id = wandb.util.generate_id()
         args.id = id
-        wandb.init(id=args.id)
+        args.logdir = os.path.join(args.logdir, args.project, args.name, f'seed_{args.seed}')
+        wandb.init(id=args.id, project=args.project, name=args.name, config=config, dir=os.path.join(args.logdir, 'wandb'))
         logger.info("`wandb` session is initialized and ready to START the operation.")
     else:
         assert args.resume, "you have provided the id, so you must be resuming an experiment, however, the resume flag is not provided"
-        wandb.init(id=args.id, resume="must")
+        args.logdir = os.path.join(args.logdir, args.project, args.name, f'seed_{args.seed}')
+        wandb.init(id=args.id, project=args.project, name=args.name, resume="must", dir=os.path.join(args.logdir, 'wandb'))
         logger.info("`wandb` session is initialized and ready to RESUME the operation.")
