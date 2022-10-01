c4d1bc0691fdadbc50da5a869ae22a634dca5697
Shayan Fazeli <shayan@cs.ucla.edu>
2022-10-01 10:37:09 -0700

    distributed resuming bugfix

--- graphite/utilities/wandb/utilities.py
+++ graphite/utilities/wandb/utilities.py
@@ -22,14 +22,13 @@ def initialize_wandb(args: argparse.Namespace, config: Dict[str, Any] = None) ->
     config: `Dict[str, Any]`, optional (default=None)
         The parsed configuration
     """
+    args.logdir = os.path.join(args.logdir, args.project, args.name, f'seed_{args.seed}')
     if not (args.distributed and args.local_rank != 0):
         if args.id is None:
             assert not args.resume, "you have chosen to resume a training process, therefore," \
                                     "please provide the corresponding wandb to resume logging."
             id = wandb.util.generate_id()
             args.id = id
-            args.logdir = os.path.join(args.logdir, args.project, args.name, f'seed_{args.seed}')
-
             if args.clean:
                 clean_folder(args.logdir)
             os.makedirs(os.path.join(args.logdir, 'ckpts'), exist_ok=True)
@@ -38,7 +37,6 @@ def initialize_wandb(args: argparse.Namespace, config: Dict[str, Any] = None) ->
             log_message(logger, "`wandb` session is initialized and ready to START the operation.", args)
         else:
             assert args.resume, "you have provided the id, so you must be resuming an experiment, however, the resume flag is not provided"
-            args.logdir = os.path.join(args.logdir, args.project, args.name, f'seed_{args.seed}')
             wandb.init(id=args.id, project=args.project, name=args.name, resume="must", dir=args.logdir)
             log_message(logger, "`wandb` session is initialized and ready to RESUME the operation.", args)
 
