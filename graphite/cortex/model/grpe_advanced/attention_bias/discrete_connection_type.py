moving node count to collate + fixing mixed precision in a module

graphite/cortex/model/grpe_advanced/attention_bias/discrete_connection_type.py
@@ -149,8 +149,8 @@ class DiscreteConnectionTypeEmbeddingAttentionBiasComplementaryValues(torch.nn.M
 
         # - placeholder
         supplementary_value = torch.zeros(
-            (batch_size, num_heads, num_nodes, self.num_connection_types)
-        ).to(attention_weights.device).float()
+            (batch_size, num_heads, num_nodes, self.num_connection_types), device=attention_weights.device
+        )
 
         # - note that edge_types is of `dim=batch_size, num_nodes, num_nodes`, therefore,
         # upon repeating we will have `dim=batch_size, num_heads, num_nodes, num_nodes` with each

graphite/cortex/model/grpe_advanced/grpe.py
@@ -191,7 +191,7 @@ class GraphRelativePositionalEncodingNetworkAdvanced(torch.nn.Module):
             The graph embeddings of `dim=(batch_size, model_dim)`.
         """
         # - getting the batched graphs
-        batched_graph = batch['graphs']
+        # batched_graph = batch['graphs']
 
         # - getting the encoded node types (with the offset applied)
         node_type = batch['node_type']
@@ -218,9 +218,8 @@ class GraphRelativePositionalEncodingNetworkAdvanced(torch.nn.Module):
         node_features = self.perturb_node_representations(node_features=node_features)
 
         # - getting the node counts (including the task node)
-        graph_node_counts = [batched_graph[i].num_nodes for i in range(len(batched_graph))]
-        graph_node_counts = torch.tensor(graph_node_counts).to(device)
-        max_node_count = max(graph_node_counts)
+        graph_node_counts = batch['node_counts']
+        max_node_count = graph_node_counts.max().item()
         mask = get_mask_from_sequence_lengths(
             graph_node_counts,
             max_length=max_node_count