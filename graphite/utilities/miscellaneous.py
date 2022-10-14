84e962721d1b80b87e911457a515dd819a1c81a0
Shayan Fazeli <shayan@cs.ucla.edu>
2022-10-13 18:23:25 -0700

    2n-1 degree of freedom  toeplitz

--- graphite/utilities/miscellaneous.py
+++ graphite/utilities/miscellaneous.py
@@ -6,9 +6,9 @@ def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-def toeplitz(c):
-    # - toeplitz matrix
-    vals = torch.cat((c, c[1:].flip(0)))
-    shape = len(c), len(c)
+# from `https://stackoverflow.com/questions/69809789/is-there-any-way-to-create-a-tensor-with-a-specific-pattern-in-pytorch`
+def toeplitz(c, r):
+    vals = torch.cat((r, c[1:].flip(0)))
+    shape = len(c), len(r)
     i, j = torch.ones(*shape).nonzero().T
     return vals[j-i].reshape(*shape)
