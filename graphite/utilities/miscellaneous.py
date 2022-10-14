895ebd8caff2bcceff0de125d8b43b5490c301ee
Shayan Fazeli <shayan@cs.ucla.edu>
2022-10-13 17:12:50 -0700

    toeplitz matrix for "your transformer is not as powerful as you think" paper

--- graphite/utilities/miscellaneous.py
+++ graphite/utilities/miscellaneous.py
@@ -4,3 +4,11 @@ import torch.nn
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def toeplitz(c):
+    # - toeplitz matrix
+    vals = torch.cat((c, c[1:].flip(0)))
+    shape = len(c), len(c)
+    i, j = torch.ones(*shape).nonzero().T
+    return vals[j-i].reshape(*shape)
